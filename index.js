91cf047f9c4bcc1295ef9738f2a21f290df2b363
Lisa Steele <lisahsteele@gmail.com>
2019-06-14 08:55:58 -0700

    Added nodemon to watch files. Also created User and Posts models

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Ciao Mondo"));
 
 app.listen(5000, () => console.log('Server is running on port 5000'));
 
