fe5322eeec2b78e0f314cc1a6f8f52f44d5695a4
Lisa Steele <lisahsteele@gmail.com>
2019-06-14 13:32:43 -0700

    Graphiql is up and running on the browser. UserType and Schema defined, and root query for Users written

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const db = require('./config/keys').mongoURI;
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressGraphQL = require('express-graphql');
+const User = require('./models/user');
+const schema = require('./schema/schema');
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Ciao Mondo"));
-
 app.listen(5000, () => console.log('Server is running on port 5000'));
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true
+  })
+);
+
+mongoose 
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
